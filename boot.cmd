6920460b767e3775e7738aa64ebd43387ab8fc8e
Herry <herry13@gmail.com>
2017-11-05 04:25:15 +0000

    disable overlaytmpfs if file 'nooverlaytmpfs' exists

--- boot.cmd
+++ boot.cmd
@@ -33,10 +33,16 @@ else
 	setenv root_dev /dev/mmcblk0p2;
 	setenv root_part "0:2";
 fi;
-mw ${env_addr_r} 0 100;
+mw ${env_addr_r} 0x20 0x100;
 env export -t ${env_addr_r} root_dev;
 fatwrite mmc 0:1 ${env_addr_r} ${env_file} 24;
-setenv bootargs 8250.nr_uarts=1 console=ttyAMA0,115200 console=tty1 noquite loglevel=7 dwc_otg.lpm_enable=0 root=${root_dev} rootfstype=ext4;
+setenv bootargs_default "8250.nr_uarts=1 console=ttyAMA0,115200 console=tty1 noquite loglevel=7 dwc_otg.lpm_enable=0 root=${root_dev} rootfstype=ext4";
+if fatload mmc 0:1 ${env_addr_r} nooverlaytmpfs;
+then
+	setenv bootargs ${bootargs_default};
+else
+	setenv bootargs "${bootargs_default} overlaytmpfs";
+fi;
 fatload mmc 0:1 0x2000000 ${fdtfile};
 ext4load mmc ${root_part} ${kernel_addr_r} ${boot_prefix}/${kernel};
 ext4load mmc ${root_part} ${ramdisk_addr_r} ${boot_prefix}/${ramdisk};
